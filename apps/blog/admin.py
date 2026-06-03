0b4b0530741842d46f5f0247281e65deb866461a
Mohammadreza <a1mmdrez@gmail.com>
2026-06-04 02:54:20 +0430

    feat: setup admin interface for Post, Category, Tag and Contact models

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Category, Tag, Post
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    search_fields = ['name', 'slug']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    search_fields = ['name', 'slug']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'author',
+        'title',
+        'view_count',
+        'status',
+        'published_date'
+    ]
+
+    search_fields = [
+        'title',
+        'content',
+        'author__username'
+    ]
+
+    list_filter = [
+        'author',
+        'status'
+    ]
+
+    date_hierarchy = 'created_date'
