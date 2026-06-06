a1ddd01f6abfa48db545ad6d7c8568fc237fa295
Mohammadreza <a1mmdrez@gmail.com>
2026-06-07 02:21:50 +0430

    feat: add category widget with post count and ordering

--- apps/blog/templatetags/blog_tags.py
+++ apps/blog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from apps.blog.models import Post
-from apps.core.models import Ad
+from apps.blog.models import Category
 register = template.Library()
 @register.inclusion_tag("blog/partials/popular_posts.html")
 def popular_posts():
@@ -12,3 +12,17 @@ def popular_posts():
 @register.inclusion_tag("blog/partials/author_widget.html")
 def author_widget(author):
     return {'author':author}
+
+
+@register.inclusion_tag("blog/partials/category_widget.html")
+def category_widget():
+    categories = Category.objects.all()
+
+    for cat in categories:
+        cat.post_count = Post.objects.published().filter(categories=cat).count()
+    # sort
+    categories = sorted(categories, key=lambda c: c.post_count, reverse=True)
+
+    return {
+        "categories": categories
+    }
