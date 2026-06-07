3e06cb7440cdc3ad5cdbd7395e119364d2266fdd
Mohammadreza <a1mmdrez@gmail.com>
2026-06-08 03:18:49 +0430

    test: add fallback for user full name

--- apps/accounts/tests/test_models.py
+++ apps/accounts/tests/test_models.py
@@ -13,3 +13,14 @@ class UserModelTest(TestCase):
         result = user.get_full_name()
 
         self.assertEqual(result, "Mohammadreza Amini")
+
+
+    def test_get_full_name_returns_email_when_names_are_empty(self):
+        user = User.objects.create_user(
+            email="test@example.com",
+            password="123456789",
+            first_name="",
+            last_name="",
+        )
+        result = user.get_full_name()
+        self.assertEqual(result, "test@example.com")
