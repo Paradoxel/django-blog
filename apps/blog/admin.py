0aa8eb2ec91826c42475c2c7dd5045e259e243b3
Mohammadreza <a1mmdrez@gmail.com>
2026-06-10 02:20:06 +0430

    fix(blog): fix admin search field and improve PostAdmin config

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,38 +1,29 @@
 from django.contrib import admin
+
 from .models import Category, Tag, Post
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    search_fields = ['name', 'slug']
+    """Admin configuration for Category model."""
+
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    search_fields = ['name', 'slug']
+    """Admin configuration for Tag model."""
+
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = [
-        'author',
-        'title',
-        'view_count',
-        'status',
-        'published_date'
-    ]
+    """Admin configuration for Post model."""
 
-    search_fields = [
-        'title',
-        'content',
-        'author__username'
-    ]
-
-    list_filter = [
-        'author',
-        'status'
-    ]
-
+    list_display = ('title', 'author', 'status', 'view_count', 'published_date')
+    search_fields = ('title', 'content', 'author__email')  
+    list_filter = ('status',)  
     date_hierarchy = 'created_date'
