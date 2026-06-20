ca7a8888464ca172be09bf7b147de58ea5c643bd
Mohammadreza <a1mmdrez@gmail.com>
2026-06-20 16:30:10 +0430

    feat: implement profile update (User + Profile) and auth redirects

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout, get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView,UpdateView
+from apps.accounts.forms import UserUpdateForm,ProfileUpdateForm
 
 from .forms import UserRegisterForm
 
@@ -47,3 +49,51 @@ def user_logout(request):
         logout(request)
         messages.success(request, "You have been logged out successfully.")
     return redirect("core:home")
+
+
+
+
+# for update the user profile 
+class UpdateUserProfile(LoginRequiredMixin,UpdateView):
+    """
+    Handle profile update for logged-in users.
+    Updates both User and Profile models in one submission.
+    """
+    template_name = "accounts/profile.html"
+    form_class=UserUpdateForm
+    def get_success_url(self):
+        return reverse_lazy("accounts:profile")
+    def get_object(self, queryset = ...):
+        # return current user
+        return self.request.user
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.method=='POST':
+            context['profile_form']= ProfileUpdateForm(
+                self.request.POST,
+                self.request.FILES, # needed for avatar
+                instance=self.get_object().profile,
+            )
+        else:
+            context["profile_form"] = ProfileUpdateForm(
+                    instance=self.get_object().profile
+            )
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        profile_form = context["profile_form"]
+
+        if form.is_valid() and profile_form.is_valid():
+            self.object = form.save()
+            profile_form.save()
+
+            messages.success(self.request, "Profile updated successfully")
+
+            return redirect(self.get_success_url())
+
+        return self.form_invalid(form)
+        
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
