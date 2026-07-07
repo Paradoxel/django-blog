ac6b9c5177e626d68c3b43e61040a7ef8b82f399
Mohammadreza <a1mmdrez@gmail.com>
2026-07-08 00:27:20 +0430

    refactor(accounts): clean up permission mixins

--- apps/accounts/permissions.py
+++ apps/accounts/permissions.py
@@ -1,11 +1,10 @@
 from django.core.exceptions import PermissionDenied
-from apps.accounts.models import UserTypes
 from django.shortcuts import redirect
+
 
 class WriterRequiredMixin:
     """
-    Restricts access to writer-only users.
-    Must be used AFTER LoginRequiredMixin.
+    Restrict access to writer-only users.
     """
 
     def dispatch(self, request, *args, **kwargs):
@@ -13,18 +12,18 @@ class WriterRequiredMixin:
             raise PermissionDenied
 
         return super().dispatch(request, *args, **kwargs)
-    
 
 
 class DeleteAccountVerificationRequiredMixin:
+    """
+    Ensure account deletion has been verified.
+    """
 
     def dispatch(self, request, *args, **kwargs):
-
         if not request.session.get("delete_account_verified"):
             return redirect("accounts:delete_account")
 
         return super().dispatch(request, *args, **kwargs)
-    
 
 
 class ReaderRequiredMixin:
